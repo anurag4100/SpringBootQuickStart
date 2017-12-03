9de037e7eba53d5b190aa4a6f45e5efec30b8781
Anurag <anuragbharatpandey@gmail.com>
2017-12-03 18:20:24 +0530

    make it bette1r

--- spring-boot-quick-start-master/src/main/java/com/codeatomic/springbootquickstart/topic/CustomerController.java
+++ spring-boot-quick-start-master/src/main/java/com/codeatomic/springbootquickstart/topic/CustomerController.java
@@ -4,16 +4,13 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
-import org.assertj.core.util.Lists;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
