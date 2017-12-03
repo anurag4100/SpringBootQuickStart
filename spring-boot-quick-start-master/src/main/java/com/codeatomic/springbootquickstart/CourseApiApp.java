e789ae0ce1b0c12fae97ec8000292dd750a80fac
Anurag <anuragbharatpandey@gmail.com>
2017-12-03 18:50:10 +0530

    make it better

--- spring-boot-quick-start-master/src/main/java/com/codeatomic/springbootquickstart/CourseApiApp.java
+++ spring-boot-quick-start-master/src/main/java/com/codeatomic/springbootquickstart/CourseApiApp.java
@@ -2,9 +2,17 @@ package com.codeatomic.springbootquickstart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
 public class CourseApiApp {
+	
+	@RequestMapping("/")
+    @ResponseBody
+    String home() {
+      return "Hello World!";
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(CourseApiApp.class, args);
