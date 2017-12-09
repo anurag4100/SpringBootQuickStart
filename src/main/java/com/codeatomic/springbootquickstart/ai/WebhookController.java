9d39b73f4749667ef59f4c96301ae6d58a9069d7
Anurag <anuragbharatpandey@gmail.com>
2017-12-09 17:25:15 +0530

    make it better

--- src/main/java/com/codeatomic/springbootquickstart/ai/WebhookController.java
+++ src/main/java/com/codeatomic/springbootquickstart/ai/WebhookController.java
@@ -1,4 +1,4 @@
-package com.codeatomic.springbootquickstart.topic;
+package com.codeatomic.springbootquickstart.ai;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -7,20 +7,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import ai.api.model.AIRequest;
-import ai.api.model.AIResponse;
-import ai.api.model.Result;
 
 @RestController
-public class LibAiController {
+public class WebhookController {
 
     
     @RequestMapping(method=RequestMethod.POST, value="/webhook/customers")
-	public ResponseEntity<AIResponse> addCustomer(@RequestBody AIRequest  aiRequest) {
-    	AIResponse resonse = new AIResponse();
+	public ResponseEntity<AICustomResponse> addCustomer(@RequestBody AICustomRequest  aiRequest) {
+    	AICustomResponse resonse = new AICustomResponse();
     	try {
     		System.out.println("inside webhook controller: "+aiRequest.getSessionId());
-    		
 		} catch (Exception e) {
 			return new ResponseEntity<>(resonse,HttpStatus.BAD_REQUEST);
 		}
