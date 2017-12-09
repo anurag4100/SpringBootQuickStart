24e0558a7c129c7e80937a6d0944ea60ad124bc3
Anurag <anuragbharatpandey@gmail.com>
2017-12-09 18:08:09 +0530

    make it better

--- src/main/java/com/codeatomic/springbootquickstart/ai/WebhookController.java
+++ src/main/java/com/codeatomic/springbootquickstart/ai/WebhookController.java
@@ -7,6 +7,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.codeatomic.springbootquickstart.topic.Address;
+import com.codeatomic.springbootquickstart.topic.Customer;
+
 
 @RestController
 public class WebhookController {
@@ -17,6 +20,13 @@ public class WebhookController {
     	AICustomResponse resonse = new AICustomResponse();
     	try {
     		System.out.println("inside webhook controller: "+aiRequest.getResult().getResolvedQuery());
+    		resonse.setDisplayText("abc");
+    		resonse.setData(new Customer("Anurag", "andey", new Address()));
+    		resonse.setLang("en");
+    		resonse.setSpeech("abcd");
+    		resonse.setId("1234");
+    		resonse.setSessionId("123456");
+    		resonse.setSource("rest");
 		} catch (Exception e) {
 			return new ResponseEntity<>(resonse,HttpStatus.BAD_REQUEST);
 		}
