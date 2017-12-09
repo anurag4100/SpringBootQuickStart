c4534b5688e881343c1358ad8a7dfdf72ec0723a
Anurag <anuragbharatpandey@gmail.com>
2017-12-09 17:58:24 +0530

    make it better

--- src/main/java/com/codeatomic/springbootquickstart/ai/AICustomRequest.java
+++ src/main/java/com/codeatomic/springbootquickstart/ai/AICustomRequest.java
@@ -1,12 +1,16 @@
 package com.codeatomic.springbootquickstart.ai;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import ai.api.model.AIRequest;
 import ai.api.model.Result;
 
-public class AICustomRequest {
+public class AICustomRequest extends AIRequest{
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	@JsonIgnore
 	private Result result;
 
 	public Result getResult() {
